99d745e54894c12ca779c1f4ecb30a4d8d217402
DenGian <97441878+DenGian@users.noreply.github.com>
2022-06-03 09:43:19 +0200

    updates vlad page

--- vlad/ian.js
+++ vlad/ian.js
@@ -1,7 +1,7 @@
 //body
-let color = document.getElementsByTagName('*')
+let color = document.getElementsByTagName('*');
 for (let i=0, max = color.length; i<max; i++){
-    document.getElementsByTagName('*')[i].style.backgroundColor = 'blue'
+    document.getElementsByTagName('*')[i].style.backgroundColor = 'blue';
 }
 console.log(color);
 
@@ -9,12 +9,11 @@ console.log(color);
 document.getElementsByTagName('h1')[0].innerText = 'Ian';
 
 // change list items characteristics
-document.getElementsByTagName('li')[2].innerText = 'rich';
+document.getElementsByTagName('p')[2].innerText = 'Helpful';
 
 for (let i=0; i<3; i++){
-    document.getElementsByTagName('li')[i].style.fontSize = '20px';
-    document.getElementsByTagName('li')[i].style.color =    'grey';
-    document.getElementsByTagName('li')[i].style.fontFamily = 'Comic sans MS , serif';
+     document.getElementsByTagName('li')[i].style.color = 'grey';
+     document.getElementsByTagName('p')[i].style.fontFamily = 'Comic sans MS , serif';
 }
 /*
 I was stupid here
@@ -31,4 +30,14 @@ document.getElementsByTagName('li')[2].style.fontFamily = 'Comic sans MS , serif
  */
 
  // image
-document.getElementsByTagName('img')[0].src='resources/ian.jpg'
+document.getElementsByTagName('img')[0].src='resources/ian.jpg';
+
+// sport
+document.getElementsByTagName('h5')[2].innerText = 'Football';
+document.getElementsByTagName('img')[3].src='resources/football.png';
+
+// character
+document.getElementsByTagName('h5')[3].innerText = 'Stewie';
+document.getElementsByTagName('img')[4].src='resources/stewie.gif';
+
+// challenge
